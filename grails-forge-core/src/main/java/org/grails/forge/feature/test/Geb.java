e24f5d0fc46b62c1a7a75c02a6a009eecd69ba09
Puneet Behl <behlp@objectcomputing.com>
2022-02-25 01:37:09 +0530

    Add Geb feature

---
+++ grails-forge-core/src/main/java/org/grails/forge/feature/test/Geb.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.grails.forge.feature.test;
+
+import jakarta.inject.Singleton;
+import org.grails.forge.application.ApplicationType;
+import org.grails.forge.application.generator.GeneratorContext;
+import org.grails.forge.build.dependencies.Dependency;
+import org.grails.forge.build.gradle.GradlePlugin;
+import org.grails.forge.feature.Category;
+import org.grails.forge.feature.DefaultFeature;
+import org.grails.forge.feature.Feature;
+import org.grails.forge.feature.FeatureContext;
+import org.grails.forge.feature.test.template.webdriverBinariesPlugin;
+import org.grails.forge.options.Options;
+import org.grails.forge.template.RockerWritable;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+@Singleton
+public class Geb implements DefaultFeature {
+
+    private final Spock spock;
+
+    public Geb(Spock spock) {
+        this.spock = spock;
+    }
+
+    @Override
+    public boolean shouldApply(ApplicationType applicationType, Options options, Set<Feature> selectedFeatures) {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "geb";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Geb Functional Testing for Grails";
+    }
+
+    @Override
+    public String getDescription() {
+        return "This plugins configure Geb for Grails framework to write automation tests.";
+    }
+
+    @Override
+    public boolean supports(ApplicationType applicationType) {
+        return true;
+    }
+
+    @Override
+    public String getCategory() {
+        return Category.SERVER;
+    }
+
+    @Override
+    public String getDocumentation() {
+        return "https://github.com/grails3-plugins/geb#readme";
+    }
+
+    @Override
+    public String getThirdPartyDocumentation() {
+        return "https://www.gebish.org/manual/current/";
+    }
+
+    @Override
+    public void processSelectedFeatures(FeatureContext featureContext) {
+        if (!featureContext.isPresent(Spock.class) && spock != null) {
+            featureContext.addFeature(spock);
+        }
+    }
+
+    @Override
+    public void apply(GeneratorContext generatorContext) {
+        generatorContext.addBuildPlugin(GradlePlugin.builder()
+                .id("com.github.erdi.webdriver-binaries")
+                .lookupArtifactId("webdriver-binaries-gradle-plugin")
+                .extension(new RockerWritable(webdriverBinariesPlugin.template(generatorContext.getProject())))
+                .build());
+
+        generatorContext.addDependency(Dependency.builder()
+                .groupId("org.grails.plugins")
+                .artifactId("geb")
+                .test());
+
+        Stream.of("api", "support", "remote-driver")
+                .map(name -> "selenium-" + name)
+                .forEach(name -> {
+                    generatorContext.addDependency(Dependency.builder()
+                            .groupId("org.seleniumhq.selenium")
+                            .lookupArtifactId(name)
+                            .test());
+                });
+
+        generatorContext.addDependency(Dependency.builder()
+                .groupId("org.seleniumhq.selenium")
+                .lookupArtifactId("selenium-chrome-driver")
+                .testRuntime());
+        generatorContext.addDependency(Dependency.builder()
+                .groupId("org.seleniumhq.selenium")
+                .lookupArtifactId("selenium-firefox-driver")
+                .testRuntime());
+    }
+}
---
+++ grails-forge-core/src/test/groovy/org/grails/forge/feature/test/GebSpec.groovy
@@ -0,0 +1,41 @@
+package org.grails.forge.feature.test
+
+import org.grails.forge.ApplicationContextSpec
+import org.grails.forge.application.ApplicationType
+import org.grails.forge.fixture.CommandOutputFixture
+import org.grails.forge.options.BuildTool
+import org.grails.forge.options.JdkVersion
+import org.grails.forge.options.Language
+import org.grails.forge.options.Options
+import org.grails.forge.options.TestFramework
+
+class GebSpec extends ApplicationContextSpec implements CommandOutputFixture {
+
+    void "test dependencies"() {
+        given:
+        final def output = generate(ApplicationType.DEFAULT, new Options(Language.GROOVY, TestFramework.SPOCK, BuildTool.GRADLE, JdkVersion.JDK_11))
+        final def buildGradle = output["build.gradle"]
+
+        expect:
+        buildGradle.contains("testImplementation(\"org.grails.plugins:geb\")")
+        buildGradle.contains("testImplementation(\"org.seleniumhq.selenium:selenium-api:4.0.0\")")
+        buildGradle.contains("testImplementation(\"org.seleniumhq.selenium:selenium-support:4.0.0\")")
+        buildGradle.contains("testImplementation(\"org.seleniumhq.selenium:selenium-remote-driver:4.0.0\")")
+        buildGradle.contains("testRuntimeOnly(\"org.seleniumhq.selenium:selenium-chrome-driver:4.0.0\")")
+        buildGradle.contains("testRuntimeOnly(\"org.seleniumhq.selenium:selenium-firefox-driver:4.0.0\")")
+    }
+
+    void "test webdriver binaries gradle configurations"() {
+        given:
+        final def output = generate(ApplicationType.DEFAULT, new Options(Language.GROOVY, TestFramework.SPOCK, BuildTool.GRADLE, JdkVersion.JDK_11))
+        final def buildGradle = output["build.gradle"]
+
+        expect:
+        buildGradle.contains("id \"com.github.erdi.webdriver-binaries\" version \"2.7\"")
+        buildGradle.contains("webdriverBinaries")
+        buildGradle.contains("chromedriver '2.32'")
+        buildGradle.contains("geckodriver '0.19.0'")
+        buildGradle.contains("iedriverserver '3.8.0'")
+        buildGradle.contains("edgedriver '86.0.601.0'")
+    }
+}
