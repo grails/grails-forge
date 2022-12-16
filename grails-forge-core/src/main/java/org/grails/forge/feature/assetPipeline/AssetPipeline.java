9a7e5376e5df2b1eba6dae3da136f6922bc94e73
Puneet Behl <behlp@objectcomputing.com>
2022-12-16 13:16:39 +0530

    Update feature asset-pipeline to add resources

--- grails-forge-core/src/main/java/org/grails/forge/feature/assetPipeline/AssetPipeline.java
+++ grails-forge-core/src/main/java/org/grails/forge/feature/assetPipeline/AssetPipeline.java
@@ -26,7 +26,13 @@ import org.grails.forge.feature.Feature;
 import org.grails.forge.feature.assetPipeline.templates.assetPipelineExtension;
 import org.grails.forge.options.Options;
 import org.grails.forge.template.RockerWritable;
+import org.grails.forge.template.URLTemplate;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.Set;
 
 @Singleton
@@ -59,6 +65,26 @@ public class AssetPipeline implements DefaultFeature {
                 .groupId("com.bertramlabs.plugins")
                 .lookupArtifactId("asset-pipeline-grails")
                 .runtime());
+
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        final Path path = Paths.get(Objects.requireNonNull(classLoader.getResource("assets")).getPath());
+        walk(generatorContext, path, "assets");
+    }
+
+    private void walk(GeneratorContext generatorContext, Path path, String baseDirPath) {
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            if (Files.exists(path)) {
+                Files.walk(path)
+                        .filter(Files::isRegularFile)
+                        .forEach(file -> {
+                            final String relativePath = path.getParent().relativize(file).toString();
+                            generatorContext.addTemplate(relativePath, new URLTemplate("grails-app/" + relativePath, classLoader.getResource(relativePath)));
+                        });
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
--- grails-forge-core/src/test/groovy/org/grails/forge/feature/assetPipeline/AssetPipelineSpec.groovy
+++ grails-forge-core/src/test/groovy/org/grails/forge/feature/assetPipeline/AssetPipelineSpec.groovy
@@ -2,9 +2,16 @@ package org.grails.forge.feature.assetPipeline
 
 import org.grails.forge.ApplicationContextSpec
 import org.grails.forge.BuildBuilder
+import org.grails.forge.application.ApplicationType
 import org.grails.forge.feature.Features
+import org.grails.forge.fixture.CommandOutputFixture
+import org.grails.forge.options.BuildTool
+import org.grails.forge.options.JdkVersion
+import org.grails.forge.options.Language
+import org.grails.forge.options.Options
+import org.grails.forge.options.TestFramework
 
-class AssetPipelineSpec extends ApplicationContextSpec {
+class AssetPipelineSpec extends ApplicationContextSpec implements CommandOutputFixture {
 
     void "test asset-pipeline-grails feature"() {
         when:
@@ -28,5 +35,44 @@ assets {
     minifyJs = true
     minifyCss = true
 }''')
+    }
+
+    void "test assets files are present"() {
+        given:
+        final Map<String, String> output = generate(ApplicationType.DEFAULT, new Options(Language.GROOVY, TestFramework.SPOCK, BuildTool.GRADLE, JdkVersion.JDK_11))
+
+        expect:
+        output.containsKey("grails-app/assets/images/advancedgrails.svg")
+        output.containsKey("grails-app/assets/images/apple-touch-icon.png")
+        output.containsKey("grails-app/assets/images/apple-touch-icon-retina.png")
+        output.containsKey("grails-app/assets/images/documentation.svg")
+        output.containsKey("grails-app/assets/images/favicon.ico")
+        output.containsKey("grails-app/assets/images/grails.svg")
+        output.containsKey("grails-app/assets/images/grails-cupsonly-logo-white.svg")
+        output.containsKey("grails-app/assets/images/slack.svg")
+        output.containsKey("grails-app/assets/images/spinner.gif")
+        output.containsKey("grails-app/assets/images/skin/database_add.png")
+        output.containsKey("grails-app/assets/images/skin/database_delete.png")
+        output.containsKey("grails-app/assets/images/skin/database_edit.png")
+        output.containsKey("grails-app/assets/images/skin/database_save.png")
+        output.containsKey("grails-app/assets/images/skin/database_table.png")
+        output.containsKey("grails-app/assets/images/skin/exclamation.png")
+        output.containsKey("grails-app/assets/images/skin/house.png")
+        output.containsKey("grails-app/assets/images/skin/information.png")
+        output.containsKey("grails-app/assets/images/skin/sorted_asc.gif")
+        output.containsKey("grails-app/assets/images/skin/sorted_desc.gif")
+        output.containsKey("grails-app/assets/javascripts/application.js")
+        output.containsKey("grails-app/assets/javascripts/bootstrap.bundle.js")
+        output.containsKey("grails-app/assets/javascripts/bootstrap.js")
+        output.containsKey("grails-app/assets/javascripts/jquery-3.5.1.js")
+        output.containsKey("grails-app/assets/javascripts/popper.js")
+        output.containsKey("grails-app/assets/stylesheets/application.css")
+        output.containsKey("grails-app/assets/stylesheets/bootstrap.css")
+        output.containsKey("grails-app/assets/stylesheets/bootstrap-grid.css")
+        output.containsKey("grails-app/assets/stylesheets/bootstrap-reboot.css")
+        output.containsKey("grails-app/assets/stylesheets/errors.css")
+        output.containsKey("grails-app/assets/stylesheets/grails.css")
+        output.containsKey("grails-app/assets/stylesheets/main.css")
+        output.containsKey("grails-app/assets/stylesheets/mobile.css")
     }
 }
