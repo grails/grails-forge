0fe43f0fdc705026d3801f5a52e4e021280fc880
Puneet Behl <behlp@objectcomputing.com>
2023-05-19 00:47:33 +0530

    Update to Grails 6.0.0-M4

--- grails-forge-core/src/main/java/org/grails/forge/util/VersionInfo.java
+++ grails-forge-core/src/main/java/org/grails/forge/util/VersionInfo.java
@@ -77,7 +77,7 @@ public class VersionInfo {
         if (grailsVersion != null) {
             return grailsVersion.toString();
         }
-        return "6.0.0-M3";
+        return "6.0.0-M4";
     }
 
     /**
