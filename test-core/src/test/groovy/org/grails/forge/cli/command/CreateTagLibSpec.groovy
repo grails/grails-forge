19ccaa908b210f3fa6bcfa8eca1e80a832862e33
Puneet Behl <behlp@objectcomputing.com>
2023-09-23 02:48:44 +0530

    Add command create-taglib

---
+++ test-core/src/test/groovy/org/grails/forge/cli/command/CreateTagLibSpec.groovy
@@ -0,0 +1,30 @@
+package org.grails.forge.cli.command
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import org.grails.forge.cli.CodeGenConfig
+import org.grails.forge.utils.CommandSpec
+
+class CreateTagLibSpec extends CommandSpec {
+
+    void "test create-taglib command"() {
+        when:
+        generateProjectWithDefaults()
+        applicationContext.createBean(CodeGenConfig.class, new CodeGenConfig())
+
+        then:
+        applicationContext.getBean(CodeGenConfig.class)
+
+        when:
+        PicocliRunner.run(CreateTagLibCommand.class, applicationContext, "simple")
+
+        then:
+        new File(dir, "grails-app/taglib/example/grails/SimpleTagLib.groovy").exists()
+        new File(dir, "src/tests/example/grails/SimpleTagLibSpec.groovy").exists()
+
+    }
+
+    @Override
+    String getTempDirectoryPrefix() {
+        return "test-app"
+    }
+}
