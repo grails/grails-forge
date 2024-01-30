e0c7a2fd75744a137d09d0edf807fd1426a59c08
Guillermo Calvo <guillermo@guillermo.dev>
2024-01-30 17:28:48 +0100

    Add test for creating a scheduled job

---
+++ grails-cli/src/test/groovy/org/grails/forge/cli/command/CreateJobCommandSpec.groovy
@@ -0,0 +1,37 @@
+package org.grails.forge.cli.command
+
+import io.micronaut.context.ApplicationContext
+import org.grails.forge.application.ApplicationType
+import org.grails.forge.cli.CodeGenConfig
+import org.grails.forge.cli.CommandFixture
+import org.grails.forge.cli.CommandSpec
+import org.grails.forge.io.ConsoleOutput
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+
+class CreateJobCommandSpec extends CommandSpec implements CommandFixture {
+
+    @Shared
+    @AutoCleanup
+    ApplicationContext beanContext = ApplicationContext.run()
+
+
+    void "test creating a job"() {
+
+        setup:
+        generateProject(ApplicationType.WEB)
+        CodeGenConfig codeGenConfig = CodeGenConfig.load(beanContext, dir, ConsoleOutput.NOOP)
+        ConsoleOutput consoleOutput = Mock(ConsoleOutput)
+        CreateJobCommand command = new CreateJobCommand(codeGenConfig, getOutputHandler(consoleOutput), consoleOutput)
+        command.jobName = "Scheduled"
+
+        when:
+        Integer exitCode = command.call()
+        File output = new File(dir, "grails-app/jobs/example/grails/ScheduledJob.groovy")
+
+        then:
+        exitCode == 0
+        output.exists()
+        1 * consoleOutput.out({ it.contains("Rendered job") })
+    }
+}
