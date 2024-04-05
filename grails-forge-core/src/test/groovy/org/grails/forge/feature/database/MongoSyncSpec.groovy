b99251fa755dc75733d375dd06c98034ad8024ed
Puneet Behl <behlp@unityfoundation.io>
2024-04-06 02:54:47 +0530

    fix(deps): update mongodb-driver-sync to 4.11.2

--- grails-forge-core/src/test/groovy/org/grails/forge/feature/database/MongoSyncSpec.groovy
+++ grails-forge-core/src/test/groovy/org/grails/forge/feature/database/MongoSyncSpec.groovy
@@ -33,7 +33,7 @@ class MongoSyncSpec extends ApplicationContextSpec implements CommandOutputFixtu
                 .render()
 
         then:
-        template.contains('implementation("org.mongodb:mongodb-driver-sync:"4.11.2")')
+        template.contains('implementation("org.mongodb:mongodb-driver-sync:4.11.2")')
         template.contains('testImplementation("org.testcontainers:mongodb")')
     }
 }
