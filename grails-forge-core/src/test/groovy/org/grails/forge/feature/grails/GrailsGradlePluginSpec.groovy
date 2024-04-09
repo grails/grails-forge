3edf55e78c6fe2378a6808f0ce647c78ce28539a
Puneet Behl <behlp@unityfoundation.io>
2024-04-09 14:44:59 +0530

    chore: bump grailsVersion to 6.2.1-SNAPSHOT

--- grails-forge-core/src/test/groovy/org/grails/forge/feature/grails/GrailsGradlePluginSpec.groovy
+++ grails-forge-core/src/test/groovy/org/grails/forge/feature/grails/GrailsGradlePluginSpec.groovy
@@ -17,7 +17,7 @@ class GrailsGradlePluginSpec extends BeanContextSpec implements CommandOutputFix
 
         then:
         gradleProps.contains("grailsGradlePluginVersion=6.1.2")
-        gradleProps.contains("grailsVersion=6.2.0")
+        gradleProps.contains("grailsVersion=6.2.1-SNAPSHOT")
     }
 
     void "test dependencies are present for buildSrc"() {
