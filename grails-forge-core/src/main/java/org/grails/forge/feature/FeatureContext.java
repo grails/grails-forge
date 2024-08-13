7f98d123f99271a8e9a9c592c707bca70d4f4995
James Fredley <jamesfredley@users.noreply.github.com>
2024-08-13 00:37:50 -0400

    make checkstyle happy

--- grails-forge-core/src/main/java/org/grails/forge/feature/FeatureContext.java
+++ grails-forge-core/src/main/java/org/grails/forge/feature/FeatureContext.java
@@ -77,7 +77,9 @@ public class FeatureContext {
         return features.stream().filter(feature -> {
             for (FeaturePredicate predicate: exclusions) {
                 if (predicate.test(feature)) {
-                    predicate.getWarning().ifPresent(message -> { throw new IllegalArgumentException(message); });
+                    predicate.getWarning().ifPresent(message -> {
+                        throw new IllegalArgumentException(message);
+                    });
                     return false;
                 }
             }
