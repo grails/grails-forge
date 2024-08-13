a8d5aeaee388fd02a012c1c8752b4a7e2f7a0c24
James Fredley <jamesfredley@users.noreply.github.com>
2024-08-13 09:52:13 -0400

    Adjust change on IOFeatureUtil.java to fix build of grails-forge-core on Java 11 (#359)
    * Reverse a prior change to fix build of grails-forge-core
    
    * Make change Java 11 and 17 friendly

--- grails-forge-core/src/main/java/org/grails/forge/util/IOFeatureUtil.java
+++ grails-forge-core/src/main/java/org/grails/forge/util/IOFeatureUtil.java
@@ -50,7 +50,8 @@ public class IOFeatureUtil {
         } else {
             final String jarPath = path.toString().substring(0, sep);
             if (jarPath.endsWith(JAR_EXTENSION)) {
-                try (FileSystem zipFs = FileSystems.newFileSystem(Paths.get(URI.create(jarPath)))) {
+                ClassLoader loader = null;
+                try (FileSystem zipFs = FileSystems.newFileSystem(Paths.get(URI.create(jarPath)), loader)) {
                     walkFiles(zipFs.getPath(path.toString().substring(sep + 1)), classLoader, addTemplate);
                 }
             }
