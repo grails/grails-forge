f5e2f83e339dbbc00fbe8e8ed3906efa774d10a2
James Fredley <jamesfredley@users.noreply.github.com>
2024-08-27 14:42:15 -0400

    Add grails-app/conf/spring/resources.groovy as a default feature (#366)
    * Add grails-app/conf/spring/resources.groovy as a default feature to align with pre Grails 6 versions

---
+++ grails-forge-core/src/test/groovy/org/grails/forge/feature/spring/SpringResourcesSpec.groovy
@@ -0,0 +1,24 @@
+package org.grails.forge.feature.spring
+
+import org.grails.forge.ApplicationContextSpec
+import org.grails.forge.application.ApplicationType
+import org.grails.forge.fixture.CommandOutputFixture
+import org.grails.forge.options.JdkVersion
+import org.grails.forge.options.Options
+import org.grails.forge.options.TestFramework
+import spock.lang.Unroll
+
+class SpringResourcesSpec extends ApplicationContextSpec implements CommandOutputFixture {
+
+    @Unroll
+    void "test /conf/spring/resources.groovy config file is present for #applicationType application"() {
+        when:
+        def output = generate(applicationType, new Options(TestFramework.SPOCK, JdkVersion.JDK_11))
+
+        then:
+        output.containsKey("grails-app/conf/spring/resources.groovy")
+
+        where:
+        applicationType << ApplicationType.values().toList()
+    }
+}
