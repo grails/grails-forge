2ee1d6bd1342ef473e1204b5236f253c7e22f5ce
James Fredley <jamesfredley@users.noreply.github.com>
2024-10-09 19:25:18 -0400

    include development, test and production dataSource configuration in generated application.yml

--- grails-forge-core/src/main/java/org/grails/forge/feature/database/DatabaseDriverConfigurationFeature.java
+++ grails-forge-core/src/main/java/org/grails/forge/feature/database/DatabaseDriverConfigurationFeature.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 original authors
+ * Copyright 2017-2024 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,13 @@ import org.grails.forge.feature.Feature;
 import java.util.Map;
 import java.util.Optional;
 
+import static org.grails.forge.feature.config.ConfigurationFeature.DEV_ENVIRONMENT_KEY;
+import static org.grails.forge.feature.config.ConfigurationFeature.TEST_ENVIRONMENT_KEY;
+import static org.grails.forge.feature.config.ConfigurationFeature.PROD_ENVIRONMENT_KEY;
+import static org.grails.forge.feature.config.ConfigurationFeature.ENVIRONMENTS_KEY;
+import static org.grails.forge.feature.config.ConfigurationFeature.PROPERTIES_KEY;
+import static org.grails.forge.feature.database.HibernateGorm.PREFIX;
+
 /**
  * A feature that configures a datasource with a driver
  */
@@ -33,14 +40,45 @@ public interface DatabaseDriverConfigurationFeature extends Feature {
 
     String getPasswordKey();
 
+    String getDbCreateKey();
+
     default void applyDefaultConfig(DatabaseDriverFeature dbFeature, Map<String, Object> config) {
-        Optional.ofNullable(dbFeature.getJdbcUrl()).ifPresent(url -> config.put(getUrlKey(), url));
         Optional.ofNullable(dbFeature.getDriverClass()).ifPresent(driver -> config.put(getDriverKey(), driver));
         Optional.ofNullable(dbFeature.getDefaultUser()).ifPresent(user -> config.put(getUsernameKey(), user));
         Optional.ofNullable(dbFeature.getDefaultPassword()).ifPresent(pass -> config.put(getPasswordKey(), pass));
+
+        config.put(ENVIRONMENTS_KEY + "." + DEV_ENVIRONMENT_KEY + "." + getDbCreateKey(), "create-drop");
+        Optional.ofNullable(dbFeature.getJdbcDevUrl()).ifPresent(url -> config.put(ENVIRONMENTS_KEY + "." + DEV_ENVIRONMENT_KEY + "." + getUrlKey(), url));
+        config.put(ENVIRONMENTS_KEY + "." + TEST_ENVIRONMENT_KEY + "." + getDbCreateKey(), "update");
+        Optional.ofNullable(dbFeature.getJdbcTestUrl()).ifPresent(url -> config.put(ENVIRONMENTS_KEY + "." + TEST_ENVIRONMENT_KEY + "." + getUrlKey(), url));
+        config.put(ENVIRONMENTS_KEY + "." + PROD_ENVIRONMENT_KEY + "." + getDbCreateKey(), "none");
+        Optional.ofNullable(dbFeature.getJdbcProdUrl()).ifPresent(url -> config.put(ENVIRONMENTS_KEY + "." + PROD_ENVIRONMENT_KEY + "." + getUrlKey(), url));
+
+        addProductionDataSourceProperties(config,"jmxEnabled", true);
+        addProductionDataSourceProperties(config,"initialSize", 5);
+        addProductionDataSourceProperties(config,"maxActive", 50);
+        addProductionDataSourceProperties(config,"minIdle", 5);
+        addProductionDataSourceProperties(config,"maxIdle", 25);
+        addProductionDataSourceProperties(config,"maxWait", 10000);
+        addProductionDataSourceProperties(config,"maxAge", 600000);
+        addProductionDataSourceProperties(config,"timeBetweenEvictionRunsMillis", 5000);
+        addProductionDataSourceProperties(config,"minEvictableIdleTimeMillis", 60000);
+        addProductionDataSourceProperties(config,"validationQuery", "SELECT 1");
+        addProductionDataSourceProperties(config,"validationQueryTimeout", 3);
+        addProductionDataSourceProperties(config,"validationInterval", 15000);
+        addProductionDataSourceProperties(config,"testOnBorrow", true);
+        addProductionDataSourceProperties(config,"testWhileIdle", true);
+        addProductionDataSourceProperties(config,"testOnReturn", false);
+        addProductionDataSourceProperties(config,"jdbcInterceptors", "ConnectionState");
+        addProductionDataSourceProperties(config,"defaultTransactionIsolation", 2);
+
         final Map<String, Object> additionalConfig = dbFeature.getAdditionalConfig();
         if (!additionalConfig.isEmpty()) {
             config.putAll(additionalConfig);
         }
+    }
+
+    default void addProductionDataSourceProperties(Map<String, Object> config, String key, Object value) {
+        config.put(ENVIRONMENTS_KEY + "." + PROD_ENVIRONMENT_KEY + "." + PREFIX + PROPERTIES_KEY + "." + key, value);
     }
 }
