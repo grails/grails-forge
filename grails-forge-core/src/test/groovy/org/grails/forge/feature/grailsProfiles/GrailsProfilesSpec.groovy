ef4fe55dc9550154e311f464a21badf769958b95
James Fredley <jamesfredley@users.noreply.github.com>
2024-10-10 09:27:58 -0400

    Set profile on all applications and add grails.codegen.defaultPackage (#388)
    * Set profile on all applications now that grail-shell has been restored and requires a profile
    
    * Add grails.codegen.defaultPackage required by profile commands

---
+++ grails-forge-core/src/test/groovy/org/grails/forge/feature/grailsProfiles/GrailsProfilesSpec.groovy
@@ -0,0 +1,40 @@
+package org.grails.forge.feature.grailsProfiles
+
+import org.grails.forge.ApplicationContextSpec
+import org.grails.forge.application.ApplicationType
+import org.grails.forge.application.generator.GeneratorContext
+import org.grails.forge.fixture.CommandOutputFixture
+import org.grails.forge.options.JdkVersion
+import org.grails.forge.options.Options
+import org.grails.forge.options.TestFramework
+import spock.lang.Unroll
+
+class GrailsProfilesSpec extends ApplicationContextSpec implements CommandOutputFixture {
+
+    @Unroll
+    void "test profile dependency is present for #applicationType application"() {
+        when:
+        def output = generate(applicationType, new Options(TestFramework.SPOCK, JdkVersion.JDK_11))
+
+        then:
+        output.containsKey("build.gradle")
+        def build = output.get("build.gradle")
+        build.contains("profile(\"org.grails.profiles:${applicationType.name.replace("_", "-")}\")")
+
+        where:
+        applicationType << ApplicationType.values().toList()
+    }
+
+    void "test config"() {
+        when:
+        def output = generate(applicationType, new Options(TestFramework.SPOCK, JdkVersion.JDK_11))
+
+        then:
+        output.containsKey("grails-app/conf/application.yml")
+        def build = output.get("grails-app/conf/application.yml")
+        build.contains("defaultPackage")
+
+        where:
+        applicationType << ApplicationType.values().toList()
+    }
+}
